e3525c1ca013e95132357e82e66d3b7bfdf3c200
R Doevelaar <rdoevelaar@users.noreply.github.com>
2015-04-01 22:21:21 +0200

    Herkenning van meerdere elementen aan reader toegevoegd. Writer-class wat netter gemaakt.

--- src/main/java/nl/tkp/XmlWriter.java
+++ src/main/java/nl/tkp/XmlWriter.java
@@ -10,46 +10,54 @@ import java.io.FileOutputStream;
 public class XmlWriter {
     private String configFile;
 
+    // create an XMLOutputFactory
+    private XMLOutputFactory outputFactory = XMLOutputFactory.newInstance();
+    // create XMLEventWriter
+    private XMLEventWriter eventWriter;
+    // create an EventFactory
+    XMLEventFactory eventFactory = XMLEventFactory.newInstance();
+    XMLEvent end = eventFactory.createDTD("\n");
+    XMLEvent tab = eventFactory.createDTD("\t");
+
     public void setFile(String configFile) {
         this.configFile = configFile;
     }
 
-    public void saveConfig() throws Exception {
-        // create an XMLOutputFactory
-        XMLOutputFactory outputFactory = XMLOutputFactory.newInstance();
-        // create XMLEventWriter
-        XMLEventWriter eventWriter = outputFactory
-                .createXMLEventWriter(new FileOutputStream(configFile));
+    // Output aanmaken als file met eerste regels.
+    public void createDocument() throws Exception {
+        eventWriter = outputFactory.createXMLEventWriter(new FileOutputStream(configFile));
         // create an EventFactory
-        XMLEventFactory eventFactory = XMLEventFactory.newInstance();
-        XMLEvent end = eventFactory.createDTD("\n");
+//        XMLEventFactory eventFactory = XMLEventFactory.newInstance();
         // create and write Start Tag
         StartDocument startDocument = eventFactory.createStartDocument();
         eventWriter.add(startDocument);
-
-        // create config open tag
-        StartElement configStartElement = eventFactory.createStartElement("",
-                "", "config");
-        eventWriter.add(configStartElement);
         eventWriter.add(end);
-        // Write the different nodes
-        createNode(eventWriter, "mode", "1");
-        createNode(eventWriter, "unit", "901");
-        createNode(eventWriter, "current", "0");
-        createNode(eventWriter, "interactive", "0");
-
-        eventWriter.add(eventFactory.createEndElement("", "", "config"));
-        eventWriter.add(end);
-        eventWriter.add(eventFactory.createEndDocument());
-        eventWriter.close();
     }
 
-    private void createNode(XMLEventWriter eventWriter, String name,
-                            String value) throws XMLStreamException {
+    public void createStartElement(String name) throws XMLStreamException {
 
-        XMLEventFactory eventFactory = XMLEventFactory.newInstance();
-        XMLEvent end = eventFactory.createDTD("\n");
-        XMLEvent tab = eventFactory.createDTD("\t");
+//        XMLEventFactory eventFactory = XMLEventFactory.newInstance();
+        // create Start node
+        StartElement sElement = eventFactory.createStartElement("", "", name);
+        eventWriter.add(tab);
+        eventWriter.add(sElement);
+        eventWriter.add(end);
+
+    }
+
+    public void createEndElement(String name) throws XMLStreamException {
+//        XMLEventFactory eventFactory = XMLEventFactory.newInstance();
+        // create End node
+        EndElement eElement = eventFactory.createEndElement("", "", name);
+        eventWriter.add(eElement);
+        eventWriter.add(end);
+
+    }
+
+    public void createNode(String name,
+                           String value) throws XMLStreamException {
+
+//        XMLEventFactory eventFactory = XMLEventFactory.newInstance();
         // create Start node
         StartElement sElement = eventFactory.createStartElement("", "", name);
         eventWriter.add(tab);
@@ -64,4 +72,13 @@ public class XmlWriter {
 
     }
 
+    public void endDocument() throws XMLStreamException {
+//        XMLEventFactory eventFactory = XMLEventFactory.newInstance();
+//        eventWriter.add(eventFactory.createEndElement("", "", "config"));
+//        eventWriter.add(end);
+        eventWriter.add(eventFactory.createEndDocument());
+        eventWriter.flush();
+        eventWriter.close();
+
+    }
 }
